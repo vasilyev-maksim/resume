503145322efec414c789158e8df6ce20b1ddf8a3
Vasilyev Maksim <jeckhummer@gmail.com>
2023-03-05 15:09:01 +0400

    bio fix

--- src/Bio.tsx
+++ src/Bio.tsx
@@ -5,7 +5,7 @@ export const Bio: React.FC = () => {
   return (
     <p className="bio">
       I'm a senior frontend developer with 8+ years of experience in creating
-      complex web platforms of all sizes (6+ year using React and Typescript, 4
+      complex web platforms of all sizes (6+ years using React and Typescript, 4
       years of backend development experience using Node.js, C# and PHP). My
       greatest accomplishment to date is the successful implementation of a
       micro-frontend architecture from scratch in a major online banking
